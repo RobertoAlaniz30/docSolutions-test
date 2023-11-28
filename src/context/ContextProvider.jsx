d4545a10d1782dc1938354f0b6afe69d89fc18bc
Rablinnn <99299901+RobertoAlaniz30@users.noreply.github.com>
2023-11-28 16:34:09 -0600

    Update Context with uppercase letters

--- src/context/ContextProvider.jsx
+++ src/context/ContextProvider.jsx
@@ -1,14 +1,14 @@
 import { createContext, useReducer } from "react";
 import { USER_STATE } from "./modules/USER/USER_STATE.JS";
 import { USER_REDUCER } from "./modules/USER/USER_REDUCER";
-export const generalContex = createContext();
+export const GeneralContex = createContext();
 
 const ContextProvider = ({ children }) => {
   const [userState, userDispatch] = useReducer(USER_REDUCER, USER_STATE);
   return (
-    <generalContex.Provider value={{ userState, userDispatch }}>
+    <GeneralContex.Provider value={{ userState, userDispatch }}>
       {children}
-    </generalContex.Provider>
+    </GeneralContex.Provider>
   );
 };
 
